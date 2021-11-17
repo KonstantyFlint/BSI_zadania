d0e8e6c691028f8a2ffc9470f5049905ab6e2685
Maciej Kucharski <s20153@pjwstk.edu.pl>
2021-11-17 18:52:59 +0100

    a

--- test_utils.py
+++ test_utils.py
@@ -7,8 +7,21 @@ import utils
 @pytest.mark.parametrize('time,failure_rate,anticipated_result',
                          [
                              (10, 0.4, lambda t: exp(-1 * 0.4 * t)),
-
+                             (1067, 0.84, lambda t: exp(-1 * 0.84 * t)),
+                             (23, 0.05, lambda t: exp(-1 * 0.05 * t)),
                          ])
-def test_z_1_a(time, failure_rate, anticipated_result):
+def test_make_reliability_function(time, failure_rate, anticipated_result):
     tested_function = utils.make_reliability_function(failure_rate)
     assert tested_function(time) == anticipated_result(time)
+
+
+# @pytest.mark.parametrize('time,failure_rate,anticipated_result',
+#                          [
+#                              (10, 0.4, lambda t: exp(-1 * 0.4 * t)),
+#                              (1067, 0.84, lambda t: exp(-1 * 0.84 * t)),
+#                              (23, 0.05, lambda t: exp(-1 * 0.05 * t)),
+#                          ])
+# def test_deduce_reliability_function(time, failure_rate):
+#     # tested_function = utils.make_reliability_function(failure_rate)
+#     # assert tested_function(time) == anticipated_result(time)
+#     pass
